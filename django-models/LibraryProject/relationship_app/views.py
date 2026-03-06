95d04dbb4726df6b020cbbfb3351ae719b138c43
Prince Oteng Twumasi <twumasiprince17@gmail.com>
2026-03-06 22:44:50 +0000

    Implemented role-based access control with UserProfile and role-specific views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -29,4 +29,22 @@ def register(request):
     else:
         form = UserCreationForm()
 
-    return render(request, "relationship_app/register.html", {"form": form})
+    return render(request, "relationship_app/register.html", {"form": form})    
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+# Admin view
+@user_passes_test(lambda u: u.userprofile.role == 'Admin')
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Librarian view
+@user_passes_test(lambda u: u.userprofile.role == 'Librarian')
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member view
+@user_passes_test(lambda u: u.userprofile.role == 'Member')
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
