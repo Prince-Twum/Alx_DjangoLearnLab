6d1aa245467d5a099a3e1f0723b92aa3cfa3493a
Prince Oteng Twumasi <twumasiprince17@gmail.com>
2026-03-06 21:24:32 +0000

    Fixed Library import in views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,11 +3,13 @@ from .models import Book, Library
 from django.views.generic.detail import DetailView
 
 
+# Function-based view
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
 
+# Class-based view
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
