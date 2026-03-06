4de6f763af4ba6e36303a2f19bdae466da46e9a9
Prince Oteng Twumasi <twumasiprince17@gmail.com>
2026-03-06 22:18:28 +0000

    Fix Django authentication: urls, views, templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -19,3 +19,13 @@ urlpatterns = [
 
     path('register/', views.register, name='register'),
 ]
+# relationship_app/urls.py
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views  # for your register view
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+]
