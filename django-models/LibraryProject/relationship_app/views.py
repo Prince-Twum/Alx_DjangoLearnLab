b8e33d1773de665722b5f2a7d666ef50183a1fdf
Prince Oteng Twumasi <twumasiprince17@gmail.com>
2026-03-06 21:31:48 +0000

    Added templates for book list and library detail

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,13 +4,11 @@ from .models import Book
 from .models import Library
 
 
-# Function-based view
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
 
-# Class-based view
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
