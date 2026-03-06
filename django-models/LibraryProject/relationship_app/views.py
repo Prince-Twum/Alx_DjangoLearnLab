5cc16a260f9371ebd31d9d9147867893832418d8
Prince Oteng Twumasi <twumasiprince17@gmail.com>
2026-03-06 21:18:16 +0000

    Fixed template paths for views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import render
 from .models import Book, Library
 from django.views.generic.detail import DetailView
 
-# Function-based view
+
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
-# Create your views here.
-# Class-based view
+
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
