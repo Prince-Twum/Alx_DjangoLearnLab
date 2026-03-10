cc17eca680227705f66c9a7bb2df7c6310a062c9
Prince Oteng Twumasi <twumasiprince17@gmail.com>
2026-03-10 15:40:36 +0000

    Added role based access control views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,22 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
-from .models import Book
-from .models import Library
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import user_passes_test
+
+from .models import Book, Library
 
 
+# List books view
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
 
+# Library detail view
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
 
-    from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
 
 # Register view
 def register(request):
@@ -29,22 +31,22 @@ def register(request):
     else:
         form = UserCreationForm()
 
-    return render(request, "relationship_app/register.html", {"form": form})    
+    return render(request, "relationship_app/register.html", {"form": form})
 
-from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
 
 # Admin view
 @user_passes_test(lambda u: u.userprofile.role == 'Admin')
 def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
+    return render(request, "relationship_app/admin_view.html")
+
 
 # Librarian view
 @user_passes_test(lambda u: u.userprofile.role == 'Librarian')
 def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
+    return render(request, "relationship_app/librarian_view.html")
+
 
 # Member view
 @user_passes_test(lambda u: u.userprofile.role == 'Member')
 def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
+    return render(request, "relationship_app/member_view.html")
